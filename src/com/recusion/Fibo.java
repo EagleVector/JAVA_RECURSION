9a2d583a4c4b593a0d5fdf88a78f93ce510b11d9
EagleVector <kumarsubhambili3@gmail.com>
2023-02-15 17:15:43 +0530

    Fibo Simplified

--- src/com/recusion/Fibo.java
+++ src/com/recusion/Fibo.java
@@ -2,17 +2,20 @@ package com.recusion;
 
 public class Fibo {
     public static void main(String[] args) {
-        int ans = fibonacci(5);
-        System.out.println(ans);
+//        int ans = fibonacci(5);
+        int answer = FiboRecurrence(25);
+        System.out.println(answer);
     }
-
-    private static int fibonacci(int n) {
-        if ( n < 2) {
-            return n;
-        }
-        else {
-            return fibonacci(n - 1) + fibonacci(n - 2);
-        }
+    static int FiboRecurrence(int n) {
+        return (int) ((Math.pow((( 1 + Math.sqrt(5)) / 2 ), n) + Math.pow((( 1 - Math.sqrt(5)) / 2 ), n)) / Math.sqrt(5));
     }
+//    static int fibonacci(int n) {
+//        if ( n < 2) {
+//            return n;
+//        }
+//        else {
+//            return fibonacci(n - 1) + fibonacci(n - 2);
+//        }
+//    }
 
 }
