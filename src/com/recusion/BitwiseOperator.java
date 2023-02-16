10a847414e32b54a4e3273d63444d24f112e970c
EagleVector <kumarsubhambili3@gmail.com>
2023-02-16 21:29:22 +0530

    Bitwise Operator

--- src/com/recusion/BitwiseOperator.java
+++ src/com/recusion/BitwiseOperator.java
@@ -2,11 +2,22 @@ package com.recusion;
 
 public class BitwiseOperator {
     public static void main(String[] args) {
-        int num = 46;
-        System.out.println(checkodd(num));
+        int[] arr = {2, 3, 3, 6, 4, 2, 4};
+//        int num = 46;
+//        System.out.println(checkodd(num));
+        int ans = findunique(arr);
+        System.out.println(ans);
     }
 
-    static boolean checkodd(int num) {
-        return ((num & 1) == 1);
+    static int findunique(int[] arr) {
+        int unique = 0;
+        for (int i : arr) {
+            unique ^= i;
+        }
+        return unique;
     }
+
+//    static boolean checkodd(int num) {
+//        return ((num & 1) == 1);
+//    }
 }
