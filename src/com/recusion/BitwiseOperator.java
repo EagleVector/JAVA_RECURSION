c39e31390b284c383ceb85bc449cbc9a60f7c51e
EagleVector <kumarsubhambili3@gmail.com>
2023-02-17 19:16:30 +0530

    Bitwise Operator

--- src/com/recusion/BitwiseOperator.java
+++ src/com/recusion/BitwiseOperator.java
@@ -9,12 +9,79 @@ public class BitwiseOperator {
 //        System.out.println(ans);
 //        int num = 3;
 //        System.out.println(magicnumber(num));
-        int num = 23432;
-        int base = 10;
+//        int num = 15;
+//        System.out.println(Integer.toBinaryString(num));
+//        int base = 10;
+//        int power = 6;
+//        int base = 3;
+//        int ans = 1;
 
-        int digcount =  (int) (Math.log(num) / Math.log(base)) + 1;
-        System.out.println(digcount);
+//        int digcount =  (int) (Math.log(num) / Math.log(base)) + 1;
+//        int answer = pascaltriangle(n);
+//        System.out.println(answer);
+//        while (power > 0) {
+//            if ((power & 1) == 1) {
+//                ans = ans * base;
+//            }
+//            base = base * base;
+//            power = power >> 1;
+//        }
+//        System.out.println(ans);
+//        int start = 0;
+//        int end = 56;
+//        int ans = xorcal(start, end, 7);
+//        System.out.println(ans);
     }
+
+//    private static int xorcal(int start, int end, int target) {
+//        while (target > start && target < end) {
+//            if (target % 4 == 0) {
+//                return target;
+//            }
+//            if (target % 4 == 2) {
+//                return target + 1;
+//            }
+//            if (target % 4 == 3) {
+//                return 0;
+//            }
+//            if (target % 4 == 1) {
+//                return 1;
+//            }
+//        }
+//        return -1;
+//    }
+
+//    private static int setbits(int num) {
+//        int count = 0;
+//        while (num > 0) {
+//            count++;
+//            num = num & (num - 1);
+//        }
+//        return count;
+//    }
+
+//    static int setbitscount(int num) {
+//        int count = 0;
+//        while (num > 0) {
+//            if ((num & 1) == 1) {
+//                count++;
+//            }
+//            num = num >> 1;
+//        }
+//        return count;
+//    }
+
+//    static boolean powerof2(int n) {
+//        if (n == 0) {
+//            return false;
+//        }
+//        return (n & (n - 1)) == 0;
+//    }
+
+//    private static int pascaltriangle(int n) {
+//        return 1 << (n - 1);
+//    }
+
 
 //    static int magicnumber(int num) {
 //        int ans = 0;
