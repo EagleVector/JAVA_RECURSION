e27ed37ee877c4b4eef792e22680e94f7cbe4cb3
EagleVector <kumarsubhambili3@gmail.com>
2023-02-18 11:59:35 +0530

    2D array patters

---
+++ src/com/recusion/Live.java
@@ -0,0 +1,52 @@
+package com.recusion;
+
+public class Live {
+    public static void main(String[] args) {
+        int[][] arr = {
+                {1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9, 10, 11, 12}
+        };
+//        waveformat(arr);
+//        vertical(arr);
+//        int[][] a2 = {
+//                {1, 2, 3},
+//                {4},
+//                {}
+//        };
+//        System.out.println(a1);
+//        System.out.println(a2);
+//        System.out.println(a2.length);
+//        System.out.println(a2[0].length);
+//        System.out.println(a2[1].length);
+//        System.out.println(a2[2].length);
+//        for (int i = 0; i < a2.length; i++) {
+//            System.out.println(a2[i]);
+//        }
+    }
+
+    //    static void vertical(int[][] arr) {
+//        for (int i = 0; i < arr[0].length; i++) {
+//            for (int j = 0; j < arr.length; j++) {
+//                System.out.print(arr[j][i] + " ");
+//            }
+//            System.out.println();
+//        }
+//    }
+//    static void waveformat(int[][] arr) {
+//
+//        for (int col = 0; col < arr[0].length; col++) {
+//
+//            if (col % 2 == 0) {
+//                for (int row = 0; row < arr.length; row++) {
+//                    System.out.print(arr[row][col] + " ");
+//                }
+//            }
+//            else {
+//                for (int row = arr.length - 1; row >= 0; row--) {
+//                    System.out.print(arr[row][col] + " ");
+//                }
+//            }
+//        }
+//    }
+}
