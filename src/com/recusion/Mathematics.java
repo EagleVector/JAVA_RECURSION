d93f8c612f27026e271b26b4b0580e8624d70076
EagleVector <kumarsubhambili3@gmail.com>
2023-02-18 12:01:34 +0530

    Mathematics

--- src/com/recusion/Mathematics.java
+++ src/com/recusion/Mathematics.java
@@ -1,2 +1,19 @@
-package com.recusion;public class Mathematics {
+package com.recusion;
+
+public class Mathematics {
+    public static void main(String[] args) {
+        int num = 5;
+        boolean answer = checkprime(num);
+        System.out.println(answer);
+    }
+
+    static boolean checkprime(int num) {
+        int n = (int) (Math.sqrt(num));
+        for(int i = 2; i <= n; i++) {
+            if (num % n == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
