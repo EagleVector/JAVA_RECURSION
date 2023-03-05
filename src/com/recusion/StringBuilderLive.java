b43a9dd80b4e174386a35f148bf46c6f8f7c0257
EagleVector <kumarsubhambili3@gmail.com>
2023-03-05 13:28:25 +0530

    Live Class Problems

--- src/com/recusion/StringBuilderLive.java
+++ src/com/recusion/StringBuilderLive.java
@@ -1,5 +1,7 @@
 package com.recusion;
 
+import java.util.Arrays;
+
 public class StringBuilderLive {
     public static void main(String[] args) {
 //        String str = "nitin";
@@ -22,8 +24,35 @@ public class StringBuilderLive {
 //        System.out.println(sb);
 //        String strng = sb.toString();
 //        System.out.println(strng);
+//        String s = "car";
+//        String t = "rat";
+//
+//        boolean ans = checkanagrams(s, t);
+//        System.out.println(ans);
+        String s = "the sky is blue";
+
+        reversalofstring(s);
 
     }
+
+    public static void reversalofstring(String s) {
+        s.trim();
+        System.out.println(Arrays.toString(s.split(" ")));
+    }
+
+//    private static boolean checkanagrams(String s, String t) {
+//
+//        if (s.length() != t.length()) {
+//            return false;
+//        }
+//        else {
+//            char[] arr1 = s.toCharArray();
+//            char[] arr2 = t.toCharArray();
+//            Arrays.sort(arr1);
+//            Arrays.sort(arr2);
+//            return Arrays.equals(arr1, arr2);
+//        }
+//    }
 
 //    public static boolean checkpalindrome(String str) {
 //        int left = 0;
