c241fe06e0b4353e1043191b5bfd07f3dff80b51
EagleVector <kumarsubhambili3@gmail.com>
2023-03-10 14:05:45 +0530

    Permutation String Questions using Recursion

--- src/com/recusion/Permutations.java
+++ src/com/recusion/Permutations.java
@@ -1,7 +1,45 @@
 package com.recusion;
 
+import java.util.ArrayList;
+import java.util.Locale;
+
 public class Permutations {
     public static void main(String[] args) {
+//        permutations("", "abc");
 
+        System.out.println(permutationList("", "abc"));
+    }
+
+//    static void permutations(String processed, String unprocessed) {
+//        if (unprocessed.isEmpty()) {
+//            System.out.println(processed);
+//            return;
+//        }
+//        char ch = unprocessed.charAt(0);
+//
+//        for (int i =0; i <= processed.length(); i++) {
+//            String first = processed.substring(0, i);
+//            String second = processed.substring(i, processed.length());
+//            permutations(first + ch + second, unprocessed.substring(1));
+//        }
+//    }
+    static ArrayList<String> permutationList(String processed, String unprocessed) {
+        if (unprocessed.isEmpty()) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(processed);
+            return list;
+        }
+        char ch = unprocessed.charAt(0);
+
+        ArrayList<String> ans = new ArrayList<>();
+
+        for (int i = 0; i <= processed.length(); i++) {
+            String first = processed.substring(0, i);
+            String second = processed.substring(i, processed.length());
+            ans.addAll(permutationList(first + ch + second, unprocessed.substring(1)));
+
+        }
+
+        return ans;
     }
 }
