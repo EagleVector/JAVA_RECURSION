58d1d608e929a954df831fab1abb438022e62bb7
EagleVector <kumarsubhambili3@gmail.com>
2023-03-10 20:39:22 +0530

    Permutation String, dice and phonepad Questions using Recursion

--- src/com/recusion/Permutations.java
+++ src/com/recusion/Permutations.java
@@ -9,9 +9,94 @@ public class Permutations {
 
 //        System.out.println(permutationList("", "abc"));
 
-        System.out.println(permutationcount("", "abc"));
+//        System.out.println(permutationcount("", "abc"));
 
+//        phonepad("", "12");
+//        System.out.println(phonepadlist("", "12"));
+//        System.out.println(phonepadcount("", "15"));
+//        diceproblem("", 4);
+        System.out.println(dicelist("", 4));
     }
+
+    public static ArrayList<String> dicelist(String processed, int target) {
+        if (target == 0) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(processed);
+            return list;
+        }
+
+        ArrayList<String> list = new ArrayList<>();
+
+        for (int i = 1; i <= 6 && i <= target; i++) {
+            list.addAll(dicelist(processed + i, target - i));
+        }
+        return list;
+    }
+
+//    public static void diceproblem(String processed, int target) {
+//        if (target == 0) {
+//            System.out.println(processed);
+//            return;
+//        }
+//
+//        for (int i = 1; i <= 6 && i <= target; i++) {
+//            diceproblem(processed + i, target - i);
+//        }
+//    }
+
+//    public static int phonepadcount(String processed, String unprocessed) {
+//        if (unprocessed.isEmpty()) {
+//            return 1;
+//        }
+//
+//        int digit = unprocessed.charAt(0) + '0';
+//        int count = 0;
+//
+//        for (int i = (digit - 1) * 3; i < (digit * 3); i++) {
+//            char ch = (char) ('a' + i);
+//
+//            count = count + phonepadcount(processed + ch, unprocessed.substring(1));
+//
+//        }
+//        return count;
+//    }
+
+//    public static ArrayList<String> phonepadlist(String processed, String unprocessed) {
+//        if (unprocessed.isEmpty()) {
+//            ArrayList<String> list = new ArrayList<>();
+//            list.add(processed);
+//            return list;
+//        }
+//
+//        int digit = unprocessed.charAt(0) - '0';
+//        ArrayList<String> list = new ArrayList<>();
+//
+//        for (int i = (digit - 1) * 3; i < digit * 3; i++) {
+//
+//            char ch = (char) ('a' + i);
+//
+//
+//            list.addAll(phonepadlist(processed + ch, unprocessed.substring(1)));
+//
+//        }
+//        return list;
+//    }
+
+//    static void phonepad(String processed, String unprocessed) {
+//        if (unprocessed.isEmpty()) {
+//            System.out.println(processed);
+//            return;
+//        }
+//
+//        int digit = unprocessed.charAt(0) - '0';
+//
+//        for (int i = (digit - 1) * 3; i < digit * 3; i++) {
+//
+//            char ch = (char) ('a' + i);
+//
+//            phonepad(processed + ch, unprocessed.substring(1));
+//        }
+//    }
 
 //    static void permutations(String processed, String unprocessed) {
 //        if (unprocessed.isEmpty()) {
@@ -46,20 +131,20 @@ public class Permutations {
 //        return ans;
 //    }
 
-    static int permutationcount(String processed, String unprocessed) {
-        if (unprocessed.isEmpty()) {
-            return 1;
-        }
-
-        char ch = unprocessed.charAt(0);
-        int count = 0;
-
-        for (int i = 0; i <= processed.length(); i++) {
-            String first = processed.substring(0, i);
-            String second = processed.substring(i, processed.length());
-            count = count + permutationcount(first + ch + second, unprocessed.substring(1));
-        }
-        return count;
-    }
+//    static int permutationcount(String processed, String unprocessed) {
+//        if (unprocessed.isEmpty()) {
+//            return 1;
+//        }
+//
+//        char ch = unprocessed.charAt(0);
+//        int count = 0;
+//
+//        for (int i = 0; i <= processed.length(); i++) {
+//            String first = processed.substring(0, i);
+//            String second = processed.substring(i, processed.length());
+//            count = count + permutationcount(first + ch + second, unprocessed.substring(1));
+//        }
+//        return count;
+//    }
 
 }
